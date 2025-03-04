3419fc17ad876b22b9e61436bee2d0cf11915555
JDLima-DS <j.davi4242@gmail.com>
2025-03-04 14:41:49 -0300

    add main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+
+def load_data(empresa):
+    
+
+
+st.write("""
+# App Preço de Ações
+O gráfico abaixo representa a evolução do preço das ações do ITAÚ (ITUB4) ao longo dos anos (2010-2024)
+""") # markdown
+
+st.write("""
+# Fim do App uau
+""") # markdown
