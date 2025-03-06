639103ab946e7ec7e28f90d606cf21a2146b0eaa
JDLima-DS <j.davi4242@gmail.com>
2025-03-05 21:05:54 -0300

    add other stocks and multiselect filter

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ import pandas as pd
 import yfinance as yf
 
 @st.cache_data
-def load_data(empresa):
-    stock_data = yf.Ticker(empresa)
-    stock_quotes = stock_data.history(period='1d', start="2010-01-01", end="2025-03-05")
-    stock_quotes = stock_quotes[["Close"]]
-    return stock_quotes
+def load_data(empresas):
+    tickers_text = " ".join(empresas)
+    stock_data = yf.Tickers(tickers_text)
+    stock_quotes = stock_data.history(start="2010-01-01", end="2025-03-05")
+    stock_quotes = stock_quotes["Close"]
+    return stock_quotes 
 
-acoes = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
-loaded_data = load_data("ITUB4.SA") # Necessita-se do .SA, devido ser uma ação brasileira(São Paulo)
+stocks = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
+loaded_data = load_data(stocks) 
 
 st.write("""
 # App Preço de Ações
-O gráfico abaixo representa a evolução do preço das ações do ITAÚ (ITUB4) ao longo dos anos (2010-2024)
+O gráfico abaixo representa a evolução do preço das ações de mercado ao longo dos anos (2010-2024)
 """) # markdown
+
+#Preparação dos filtros/Visualizações
 
 #gráfico das views entra aqui
+
+stock_list = st.multiselect("Escolha uma ação!", loaded_data.columns)
+if stock_list:
+    loaded_data = loaded_data[stock_list]
+
 st.line_chart(loaded_data)
 
-st.write("""
+#st.write("""
 # Fim do App 
-""") # markdown
+#""") # markdown
