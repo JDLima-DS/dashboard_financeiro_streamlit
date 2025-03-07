249057a3b305ae2ab0def5be62139cdfb887d545
JDLima-DS <j.davi4242@gmail.com>
2025-03-06 21:53:28 -0300

    add sidebar and some code comments

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+#Bibliotecas necessárias
+
 import streamlit as st
 import pandas as pd
 import yfinance as yf
 
+#Função de carregamento de dados(Extract -> Yahoo Finance)
 @st.cache_data
 def load_data(empresas):
     tickers_text = " ".join(empresas)
@@ -10,9 +13,11 @@ def load_data(empresas):
     stock_quotes = stock_quotes["Close"]
     return stock_quotes 
 
+#Definição das ações desejadas e carregamento dos dados em si 
 stocks = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
 loaded_data = load_data(stocks) 
 
+#Título de Início da aplicação
 st.write("""
 # App Preço de Ações
 O gráfico abaixo representa a evolução do preço das ações de mercado ao longo dos anos (2010-2024)
@@ -20,15 +25,20 @@ O gráfico abaixo representa a evolução do preço das ações de mercado ao lo
 
 #Preparação dos filtros/Visualizações
 
-#gráfico das views entra aqui
+#Criação da Sidebar
+st.sidebar.header("Filtros")
 
-stock_list = st.multiselect("Escolha uma ação!", loaded_data.columns)
+#Filtro de Ações/Stocks
+stock_list = st.sidebar.multiselect("Escolha uma ação!", loaded_data.columns)
 if stock_list:
     loaded_data = loaded_data[stock_list]
     if len(stock_list) == 1:
         unique_stock = stock_list[0]
         loaded_data = loaded_data.rename(columns = {unique_stock: "Close"})
 
+#Filtro de Datas
+
+#Gráfico de linha das views entra aqui
 st.line_chart(loaded_data)
 
 #st.write("""
